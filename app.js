eb05e3eef7522bb7a98bcff03f1f3a88a93c78c1
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-08-08 23:18:00 -0700

    error solved

--- app.js
+++ app.js
@@ -228,11 +228,11 @@
 var num = prompt("Enter number");
 
 if(num % 2 == 0){
-    console.log("The number is even");
+    document.write("The number is even");
 }
 
 else{
-    console.log("The number is odd");
+    document.write("The number is odd");
 }
 
 
